220f1e2ceea2aba241d35d394a8a86f6a691bd66
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2014-03-06 13:44:12 +0530

    Release version 0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='virtualenvwrapper.tryton',
-    version='0.2',
+    version='0.3',
 
     description="Tryton virtualenvwrapper plugin",
     long_description=open('README.rst').read(),
